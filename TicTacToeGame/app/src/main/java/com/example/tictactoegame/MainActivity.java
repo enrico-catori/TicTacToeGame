ebf60291171a764571e2e574e24a6a9ec90ae305
enrico-catori <enrico.catori@gmail.com>
2022-09-26 23:56:43 -0300

    Finishing the project

--- TicTacToeGame/app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ TicTacToeGame/app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -6,24 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button botaoIniciar;
+    EditText editJogadorUm, editJogadorDois;
+    String nomeJogador1, nomeJogador2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editJogadorUm = findViewById(R.id.editTextTextPlayer1);
+        editJogadorDois = findViewById(R.id.editTextTextPlayer2);
+
         botaoIniciar = findViewById(R.id.buttonStart);
         botaoIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent jogo = new Intent(getApplicationContext(), TicTacToeScreen.class);
+                nomeJogador1 = editJogadorUm.getText().toString();
+                nomeJogador2 = editJogadorDois.getText().toString();
+                jogo.putExtra("Value1", nomeJogador1);
+                jogo.putExtra("Value2", nomeJogador2);
                 startActivity(jogo);
             }
         });
-
-
     }
 }
--- TicTacToeGame/app/src/main/java/com/example/tictactoegame/TicTacToeScreen.java
+++ TicTacToeGame/app/src/main/java/com/example/tictactoegame/TicTacToeScreen.java
@@ -2,25 +2,30 @@ package com.example.tictactoegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TicTacToeScreen extends AppCompatActivity implements View.OnClickListener {
-    Button Botaoencerrar;
-    private TextView jogadorUmScore, jogadorDoisScore, Status;
+    private Button endButton;
+    private TextView PlayerOne, PlayerTwo;
+    private String namePlayerOne, namePlayerTwo;
+
+    private TextView playerOneScore, playerTwoScore, playerStatus;
     private Button [] buttons = new Button[9];
-    private Button encerrarJogo;
+    private Button resetGame;
 
-    private int jogadorUmContagem, jogadorDoisContagem;
-    private int contagem;
-    boolean jogadorAtivo;
+    private int playerOneScoreCount, playerTwoScoreCount, rountCount;
+    boolean activePlayer;
 
-    int [] estadoJogo = {2,2,2,2,2,2,2,2,2};
+    int [] gameState = {2,2,2,2,2,2,2,2,2};   // Estado das posições
 
-    int [][] posicoesVencedoras = {
+    int [][] winningPositions = {
             {0,1,2}, {3,4,5}, {6,7,8},
             {0,3,6}, {1,4,7}, {2,5,8},   // Posições possíveis de vitória
             {0,4,8}, {2,4,6}
@@ -31,12 +36,23 @@ public class TicTacToeScreen extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe_screen);
 
-        jogadorUmScore = (TextView) findViewById(R.id.jogadorUmScore);
-        jogadorDoisScore = (TextView) findViewById(R.id.jogadorDoisScore);
-        Status = (TextView) findViewById(R.id.Status);
+        PlayerOne = findViewById(R.id.jogadorUm);
+        PlayerTwo = findViewById(R.id.jogadorDois);
 
-        encerrarJogo = (Button) findViewById(R.id.encerrarJogo);
+        namePlayerOne = getIntent().getExtras().getString("Value1");
+        namePlayerTwo = getIntent().getExtras().getString("Value2");
 
+        // Trazendo os nomes dos jogadoras da outra activity
+        PlayerOne.setText(namePlayerOne);
+        PlayerTwo.setText(namePlayerTwo);
+
+        playerOneScore = (TextView) findViewById(R.id.jogadorUmScore);
+        playerTwoScore = (TextView) findViewById(R.id.jogadorDoisScore);
+        playerStatus = (TextView) findViewById(R.id.Status);
+
+        resetGame = (Button) findViewById(R.id.encerrarJogo);
+
+        // Armazenando os botões no array 'buttons'
         for(int i=0; i < buttons.length; i++) {
             String buttonId = "btn_" + i;
             int resourceId = getResources().getIdentifier(buttonId, "id", getPackageName());
@@ -44,14 +60,14 @@ public class TicTacToeScreen extends AppCompatActivity implements View.OnClickLi
             buttons[i].setOnClickListener(this);
         }
 
-        contagem = 0;
-        jogadorUmContagem = 0;
-        jogadorDoisContagem = 0;
-        jogadorAtivo = true;
+        rountCount = 0;
+        playerOneScoreCount = 0;   // Iniciando os contadores
+        playerTwoScoreCount = 0;
+        activePlayer = true;
 
         // Ação de retornar pra activity inicial
-        Botaoencerrar = findViewById(R.id.encerrarJogo);
-        Botaoencerrar.setOnClickListener(new View.OnClickListener() {
+        endButton = findViewById(R.id.encerrarJogo);
+        endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
@@ -60,7 +76,86 @@ public class TicTacToeScreen extends AppCompatActivity implements View.OnClickLi
     }
 
     @Override
-    public void onClick(View view) {
-        Log.i("test", "clicado");
+    public void onClick(View v) {
+        if(!((Button)v).getText().toString().equals("")) {   // Não permite que o botão seja pressionado novamente
+            return;
+        }
+        String buttonId = v.getResources().getResourceEntryName(v.getId());
+        int gameStatePointer = Integer.parseInt(buttonId.substring(buttonId.length()-1, buttonId.length()));
+
+        // Setando os ícones para o jogador ativo
+        if(activePlayer) {
+            ((Button) v).setText("X");
+            ((Button) v).setTextColor(Color.parseColor("#e63946"));
+            gameState[gameStatePointer] = 0;
+        } else {
+            ((Button) v).setText("O");
+            ((Button) v).setTextColor(Color.parseColor("#1d3557"));
+            gameState[gameStatePointer] = 1;
+        }
+        rountCount++;
+
+        // Verifica o ganhador
+        if(checkWinner()) {
+            if(activePlayer) {
+                playerOneScoreCount++;
+                updatePlayerScore();
+                String winner = namePlayerOne + " ganhou!";
+                Toast.makeText(this, winner, Toast.LENGTH_SHORT).show();
+                playAgain();
+            } else {
+                playerTwoScoreCount++;
+                updatePlayerScore();
+                String winner = namePlayerTwo + " ganhou!";
+                Toast.makeText(this, winner, Toast.LENGTH_SHORT).show();
+                playAgain();
+            }
+        } else if(rountCount == 9) {   // Empate
+            playAgain();
+            Toast.makeText(this, "Empatou", Toast.LENGTH_SHORT).show();
+        } else {
+            activePlayer = !activePlayer;   // Continua
+        }
+        // Verifica quem está ganhando
+        if(playerOneScoreCount > playerTwoScoreCount) {
+            String winning = namePlayerOne + " está ganhando!";
+            playerStatus.setText(winning);
+        } else if(playerTwoScoreCount > playerOneScoreCount) {
+            String winning = namePlayerTwo + " está ganhando!";
+            playerStatus.setText(winning);
+        } else {
+            playerStatus.setText("");
+        }
+    }
+
+    // Verifica se há ganhador
+    public boolean checkWinner() {
+        boolean winnerResult = false;
+
+        for(int [] winningPosion : winningPositions) {
+            if(gameState[winningPosion[0]] == gameState[winningPosion[1]] &&
+                    gameState[winningPosion[1]] == gameState[winningPosion[2]] &&
+                        gameState[winningPosion[0]] != 2) {
+                winnerResult = true;
+            }
+        }
+        return winnerResult;
+    }
+
+    // Atualiza os scores
+    public void updatePlayerScore() {
+        playerOneScore.setText((Integer.toString(playerOneScoreCount)));
+        playerTwoScore.setText((Integer.toString(playerTwoScoreCount)));
+    }
+
+    // Inicia um novo jogo
+    public void playAgain() {
+        rountCount = 0;
+        activePlayer = true;
+
+        for(int i=0; i < buttons.length; i++) {
+            gameState[i] = 2;
+            buttons[i].setText("");
+        }
     }
 }
